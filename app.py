33dfc897cdb9aeb2e55beb56e05599b34bad57fc
nagasainannapaneni <56877046+nagasainannapaneni@users.noreply.github.com>
2020-04-16 03:07:25 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class student(Resource):
+items = []
+
+class Item(Resource):
     def get(self, name):
-        return {'student': name}
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404
 
-api.add_resource(student, '/student/<string:name>')
+    def post(self, name):
+            data = request.get_json()
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+            return item, 201
 
-app.run(port=5000)
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
