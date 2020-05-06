c5981b1daa25f8ef590ea079166ee7ff2540a71b
nagasainannapaneni <56877046+nagasainannapaneni@users.noreply.github.com>
2020-05-06 21:54:40 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'nagasai'
 api = Api(app)
 
 items = []
@@ -11,20 +15,34 @@ class Item(Resource):
         for item in items:
             if item['name'] == name:
                 return item
-        return {'item': None}, 404
+        return {'item' : None}, 404
 
     def post(self, name):
-            data = request.get_json()
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-            return item, 201
+        data = request.get_json()
+        item = {'name' : name, 'price' : data['price']}
+        items.append(item)
+        return item, 201
+    def put(self, name):
+        parser =  reqparse.RequestParser()
+        parser.add_argument('price',
+            type = float,
+            required = True,
+            help = 'This field cannot be left blank!'
+        )
 
-
-class ItemList(Resource):
+class Itemlist(Resource):
     def get(self):
-        return {'items': items}
+        return {'items' : items}
 
 api.add_resource(Item, '/item/<string:name>')
-api.add_resource(ItemList, '/items')
+api.add_resource(Itemlist, '/items')
 
-app.run(port=5000, debug=True)
+jwt = JWT(app, authenticate, identity)
+
+@app.route('/auth')
+@jwt_required()
+def auth():
+    return '%s' % current_identity
+
+if __name__ == '__main__':
+     app.run(port=5000, debug=True)
