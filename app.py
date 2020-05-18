f84535ae124e405005be393a1123441c07916843
nagasainannapaneni <56877046+nagasainannapaneni@users.noreply.github.com>
2020-05-18 19:23:37 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required, current_identity
+
+from security import authenticate, identity
+from user import UserRegister
+from item import Item
+
+
+app = Flask(__name__)
+
+app.secret_key = 'sravani'
+
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity) #JWT object is created and after that Flask_JWT registers has an endpoint
+
+api.add_resource(Item, '/<name>')
+api.add_resource(UserRegister, '/register')
+
+if __name__ == '__main__':
+    app.run(debug=True)
